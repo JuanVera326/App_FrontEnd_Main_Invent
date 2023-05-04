b83a3383d2123af3ae2c4082d6ae45c3a6c85fb0
JuanVera326 <jjvera86@misena.edu.co>
2023-05-04 16:28:12 -0500

    Pruebas de produccion 3

--- src/helpers/api/Auth.js
+++ src/helpers/api/Auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "https://83d6-152-200-157-110.ngrok-free.app/api";  //Production
+const URL = "https://6d13-152-200-157-110.ngrok-free.app/api";  //Production
 // const URL = "http://localhost:9090/api"; //LocalHost
 //const KEY = "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==";
 
--- src/helpers/api/ElectricosRequests.js
+++ src/helpers/api/ElectricosRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://83d6-152-200-157-110.ngrok-free.app/api";  //Production
+const URL = "https://6d13-152-200-157-110.ngrok-free.app/api";  //Production
 // const URL = "http://localhost:9090/api"; //LocalHost                           
 const KEY = "bWF0aWFzLm1hL25zZnc=";
 
